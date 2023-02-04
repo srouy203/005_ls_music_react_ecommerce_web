918372aef555f2b1c1efcf152dd7092bfcd3ff0d
angleangsrouy <leangsrouyang@gmail.com>
2023-02-04 23:14:28 +0700

    V0.1

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+           <Route path='/' exact element={<Home />} />
+            <Route path='/services' element={<Services/>}/>
+            <Route path='/products' element={<Products/>}/>
+            <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+         
+
+      </Router>
+    </>
+  );
+}
+
+export default App;
+//41
